79abc146f7bda2ae2d3446b313402cf3da4b1a3c
Michael Anderson <michanders@msn.com>
2018-03-09 21:28:05 +0000

    automatically backed up by learn

--- password.js
+++ password.js
@@ -1,3 +1,3 @@
 const PasswordGenerator = require('password-generator-js')
-let password = PasswordGenerator.generatePassword([a, b, c])
+let password = PasswordGenerator.generatePassword([options, [randomFunction]])
 console.log(password)
